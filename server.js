d5f210a32bb277fffe9e1529abe2efa4def11c7e
Juan Marquez <juanmarquez4@gmail.com>
2022-08-04 14:52:53 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 // here are the dependencies
 const express = require('express');
 const path = require ('path');
+const data = require('./db/db.json')
 
 // here express is initialized
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = 3001;
+
 
 // here is the Middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-// routes
-app.get('/',(req,res) => res.send('Navigate to /index or /notes'));
+// html routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.get('/index', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+// API routes
+app.get('/api/notes', (req, res) => res.json(data));
+
+app.post ('/api/notes', (req, res) => { })
 
 // here is the listener 
 app.listen(PORT, () => 
-console.log(`Example app listening at http:localhost${PORT}`)
+console.log(`Example app listening at http://localhost:${PORT}`)
 );
 
